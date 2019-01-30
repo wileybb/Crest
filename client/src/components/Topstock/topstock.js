baa03d5051c9edeb7890d8027b105d4e43b86ebb
rskanb <rs.kanbi@gmail.com>
2019-01-30 13:49:03 -0800

    Appending one stock data to UL LI from AAPL, FB, GOOGL

--- client/src/components/Topstock/topstock.js
+++ client/src/components/Topstock/topstock.js
@@ -1,34 +1,50 @@
 import React, {Component} from "react";
 import socketIOClient from "socket.io-client";
+// import Conditiontable from "./condition.js"
+
+//stock: ["spy","dai","ndaq","iwm"]
 
 export default class Topstock extends Component {
-    constructor() {
-        super();
-        this.state = {
-          response: [],
+    state = {
+          response:[],
+          APPL:0,
+          GOOGL:0,
+          stock: ["aapl", "googl", "fb"],
           endpoint: "https://ws-api.iextrading.com/1.0/tops"
-        };
-      }
+        }
     componentDidMount(){
         const{endpoint} = this.state;
         const socket = socketIOClient(endpoint);
         socket.on('connect', () => {
             // Subscribe to topics (i.e. appl,fb,aig+)
             //socket.on('message', message => console.log(message))
-            socket.emit('subscribe', 'snap,fb,aapl,googl')
+            socket.emit('subscribe', this.state.stock.join(","));
             // Unsubscribe from topics (i.e. aig+)
             //socket.emit('unsubscribe', 'aig+')
             //console.log(response);
           })
           socket.on('message', (message) => {
-              this.setState({response:message})
-              console.log(message)
+              //let livesymbol = JSON.parse(message);
+              // console.log(livesymbol)
+              this.setState({response:JSON.parse(message)});
+              // if(livesymbol.symbol === "APPL"){
+              //   this.setState({APPL:livesymbol.lastSalePrice});
+              // } else if(livesymbol.symbol === "GOOGL"){
+              //   this.setState({GOOGL:livesymbol.lastSalePrice});
+              // }
+              console.log(this.state.response);
+              //console.log(this.state.symbol)
             })
-        //socket.on("FromAPI", data => this.setState({ response: data }));
        }
 
     render(){
-      const {response} = this.state;
+      //const response = {};
+      const response = this.state;
+      const APPL =this.state;
+      const GOOGL= this.state;
+      // const {symbol} = this.state;
+      // console.log(this.state.response);
+      //console.log(this.state.response.symbol + this.state.response.lastSalePrice);
       return(
             <div style={{ textAlign: "center" }}>
         {response
@@ -37,6 +53,9 @@ export default class Topstock extends Component {
               {/* Object.values(this.state.response).map({response} => {<div > */}
                 {/* this.state.response.map((res) => { */}
                 <div>Live Stock Price available in console log</div>
+                <ul>
+                  <li>{this.state.response.symbol}: {this.state.response.lastSalePrice}</li>
+                </ul>
                 {/* }) */}
               {/* </div>})  */}
             </div>)
