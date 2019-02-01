dc6616dac38da8f1bd9dad9f52fd6c1e3b199a42
rskanb <rs.kanbi@gmail.com>
2019-01-31 16:19:23 -0800

    axios request for alphavantage api

--- client/src/components/Topstock/topstock.js
+++ client/src/components/Topstock/topstock.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import socketIOClient from "socket.io-client";
+import API from "../../utils/API.js";
 // import Conditiontable from "./condition.js"
 
 //stock: ["spy","dai","ndaq","iwm"]
@@ -7,19 +8,15 @@ var stockArr =[];
 export default class Topstock extends Component {
     state = {
           stockResponse:[],
+          APPL:{},
+          GOOGL:{},
+          AMZN:{},
           stock: ["spy","dai","ndaq","iwm","aapl", "googl", "fb"],
           endpoint: "https://ws-api.iextrading.com/1.0/tops",
         }
     componentDidMount(){
         const{endpoint} = this.state;
         const socket = socketIOClient(endpoint);
-        // socket.on('message', (message) => {
-        //   //this.state.stockResponse.empty();
-        //   let livesymbol = JSON.parse(message)
-        //   stockArr.push(livesymbol);
-        //   this.setState({stockResponse:stockArr})
-        //     //console.log((JSON.parse(message)));
-        //   })
         socket.on('connect', () => {
             // Subscribe to topics (i.e. appl,fb,aig+)
             //socket.on('message', message => console.log(message))
@@ -31,10 +28,45 @@ export default class Topstock extends Component {
               let livesymbol = JSON.parse(message)
               stockArr.push(livesymbol);
               this.setState({stockResponse:stockArr});
-              // stockArr.length = 0;
+               stockArr.length = 0;
                 //console.log((JSON.parse(message)));
               })
           })
+            // API.alphaVantageCall().then((res) => {
+            //     console.log(res)
+            // });  
+        // const{endpoint} = this.state;
+        // const socket = socketIOClient(endpoint);
+        // socket.on('connect', () => {
+        //     // Subscribe to topics (i.e. appl,fb,aig+)
+        //     //socket.on('message', message => console.log(message))
+        //     socket.emit('subscribe', 'appl,googl,msft')
+        //     // Unsubscribe from topics (i.e. aig+)
+        //     //socket.emit('unsubscribe', 'aig+')
+        //     socket.on('message', (message) => {
+        //       //this.state.stockResponse.empty();
+        //       let liveappl = JSON.parse(message)
+        //       //stockArr.push(livesymbol);
+        //       this.setState({APPL:liveappl});
+        //        //stockArr.length = 0;
+        //         //console.log((JSON.parse(message)));
+        //       })
+        //   })  
+        //   socket.on('connect', () => {
+        //     // Subscribe to topics (i.e. appl,fb,aig+)
+        //     //socket.on('message', message => console.log(message))
+        //     socket.emit('subscribe', 'googl')
+        //     // Unsubscribe from topics (i.e. aig+)
+        //     //socket.emit('unsubscribe', 'aig+')
+        //     socket.on('message', (message) => {
+        //       //this.state.stockResponse.empty();
+        //       let livegoogl = JSON.parse(message)
+        //       //stockArr.push(livesymbol);
+        //       this.setState({GOOGL:livegoogl});
+        //        //stockArr.length = 0;
+        //         //console.log((JSON.parse(message)));
+        //       })
+        //   })     
        }
 
     render(){
@@ -59,6 +91,16 @@ export default class Topstock extends Component {
                       </li>
                   )})}
           </ul>
+          {/* <ul className="list-group">
+                      <li key={this.state.APPL.symbol} className="list-group-item">
+                          <h3><span>{this.state.APPL.symbol}</span></h3>
+                          <p><span>{this.state.APPL.lastSalePrice}</span></p>
+                      </li>
+                      <li key={this.state.GOOGL.symbol} className="list-group-item">
+                          <h3><span>{this.state.GOOGL.symbol}</span></h3>
+                          <p><span>{this.state.GOOGL.lastSalePrice}</span></p>
+                      </li>
+          </ul> */}
         </div>
            )
          : <div>Loading...</div>}
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -17,4 +17,7 @@ export default {
   singleStock: function(symbol){
     return axios.get(`https://api.iextrading.com/1.0/stock/${symbol}/batch?types=quote,news,chart&range=1m&last=10`)
   },
+  alphaVantageCall: function(){
+    return axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=SK4M0EMARF3A00VS`)
+  },
 };
