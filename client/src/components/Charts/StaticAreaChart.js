67222476cfce4353f79190645f88b1bd6043bc8c
Jordan Larios <jolarios96@gmail.com>
2019-02-03 13:07:26 -0800

    fixed chart  rerender bug

--- client/src/components/Charts/StaticAreaChart.js
+++ client/src/components/Charts/StaticAreaChart.js
@@ -4,22 +4,21 @@ import ReactFC from 'react-fusioncharts';
 import FusionCharts from 'fusioncharts';
 
 // Step 4 - Including the chart type
-// import ScrolLArea2d from 'fusioncharts/fusioncharts.charts';
+import ScrollArea2d from 'fusioncharts/fusioncharts.charts';
 
 // Step 5 - Including the theme as fusion
 import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 
 // Step 6 - Adding the chart as dependency to the core fusioncharts
-ReactFC.fcRoot(FusionCharts, /*ScrolLArea2d,*/ FusionTheme);
-
-
+ReactFC.fcRoot(FusionCharts, ScrollArea2d, FusionTheme);
 
 
 // Step 8 - Creating the DOM element to pass the react-fusioncharts component
 class StaticAreaChart extends Component {
-  constructor(props) {
-    super(props);
-    const chart = props.data.chart;
+  // state = {};
+
+  render() {
+    const chart = this.props.data.chart;
     const dataset = [];
     const dates = [];
     let yAxisMin = null;
@@ -34,63 +33,49 @@ class StaticAreaChart extends Component {
       yAxisMax = (chart[key].high > yAxisMax) ? chart[key].high : yAxisMax;
       yAxisMin = (yAxisMin === null || chart[key].low < yAxisMin) ? Math.round(chart[key].low) : yAxisMin;
 
-    })
+    });
 
 
+    this.state = {
+      type: 'scrollarea2d',// The chart type
+      width: '100%', // Width of the chart
+      height: '300', // Height of the chartf
+      dataFormat: 'json', // Data type
+      dataSource: {
+        // Chart Configuration
+        "chart": {
+          "caption": this.props.data.quote.symbol,
+          "subCaption": this.props.data.quote.companyName,
+          "xAxisName": "Date (mm-dd)",
+          "yAxisName": "Cost USD",
+          "yAxisMaxValue": yAxisMax,
+          "yAxisMinValue": yAxisMin,
+          "numberPrefix": "$",
+          "lineThickness": "3",
+          "flatScrollBars": "1",
+          "scrollheight": "10",
+          "numVisiblePlot": "6",
 
-    this.state = ({
-      chartConfigs: {
-        type: 'scrollarea2d',// The chart type
-        width: '100%', // Width of the chart
-        height: '300', // Height of the chart
-        dataFormat: 'json', // Data type
-        dataSource: {
-          // Chart Configuration
-          "chart": {
-            "caption": props.data.quote.symbol,
-            "subCaption": props.data.quote.companyName,
-            "xAxisName": "Date (mm-dd)",
-            "yAxisName": "Cost USD",
-            "yAxisMaxValue": yAxisMax,
-            "yAxisMinValue": yAxisMin,
-            "numberPrefix": "$",
-            "lineThickness": "3",
-            "flatScrollBars": "1",
-            "scrollheight": "10",
-            "numVisiblePlot": "6",
+          "numVDivLines": this.props.data.chart.length - 1,
 
-            "numVDivLines": chart.length-1,
+          "theme": "fusion",
+        },
+        "categories": [{
+          "category": dates
+        }],
+        // Chart Data
+        "dataset": [{
+          "data": dataset
+        }]
+      }
 
-            "theme": "fusion",
-          },
-          "categories": [{
-            "category": dates
-          }],
-          // Chart Data
-          "dataset": [{
-            "data": dataset
-          }]
-        }
-
-      },
-      // message: chart
-
-      // message: newData
-
-      // message: alt
-
-      // message: props.data.chart
-    })
-  }
-
-
-  render() {
-    // console.log(this.state.chartConfigs)
+    }
     return (
       <ReactFC
-        {...this.state.chartConfigs} />
+        {...this.state} />
     );
   }
 };
 
 export default StaticAreaChart;
+// ReactDOM.render(<StaticAreaChart />, document.getElementById('app'));
