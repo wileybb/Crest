aa5311339b672226ababaadb0725cb69fb31fcad
Jordan Larios <jolarios96@gmail.com>
2019-02-05 13:26:36 -0800

    small fix

--- client/src/components/Portfolio/FolioCharts.js
+++ client/src/components/Portfolio/FolioCharts.js
@@ -4,15 +4,16 @@ import FolioBar from "../Charts/FolioBar.js";
 import FolioDoughnut from "../Charts/FolioDoughnut.js";
 import FolioMultiLine from "../Charts/FolioMultiLine.js";
 
-class FolioCharts extends Component {
+class FolioCharts extends Component {    
     render() {
 
         return (
             <div>
 
                 <div class="btn-group" role="group" aria-label="Basic example">
-                    <button type="button" class="btn btn-primary">Left</button>
+                    <button onclick={console.log("hello")} type="button" class="btn btn-primary">Left</button>
                     <button type="button" class="btn btn-primary">Middle</button>
+                    <button type="button" class="btn btn-primary">Right</button>
                     <button type="button" class="btn btn-primary">Right</button>
                 </div>
 
