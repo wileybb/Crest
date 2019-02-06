ab2072c59cc3aaaf836020dc081b26390ca1c962
Jordan Larios <jolarios96@gmail.com>
2019-02-05 23:56:02 -0800

    working portfolio pie chart

--- client/src/components/Charts/FolioPie.js
+++ client/src/components/Charts/FolioPie.js
@@ -12,43 +12,45 @@ import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
 // Step 6 - Adding the chart as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Pie2d, FusionTheme);
 
-class FolioPieChart extends React.Component {
-    render() {
-        this.state = {
+class FolioPie extends React.Component {
+
+    componentDidMount() {
+        const data = [];
+        const res = this.props.data;
+        console.log(this.props.data)
+        Object.keys(res).forEach(function (key) {
+
+            data.push({
+                "label": res[key].symbol.toUpperCase(),
+                "value": res[key].latestPriceIEX
+            })
+        })
+
+        console.log("DATA ARRAY: ", data);
+
+        this.setState({
             type: 'pie2d',// The chart type
             width: this.props.width, // Width of the chart
             height: this.props.height, // Height of the chartf
             dataFormat: 'json', // Data type
             dataSource: {
                 "chart": {
-                    "caption": "Market Share of Web Servers",
-                    "plottooltext": "<b>$percentValue</b> of web servers run on $label servers",
+                    "caption": "% Net Worth",
+                    "sub-caption": "",
+                    "plottooltext": "<b>$percentValue</b> $label Stocks at $value",
                     "showlegend": "1",
                     "showpercentvalues": "1",
                     "legendposition": "bottom",
                     "usedataplotcolorforlabels": "1",
                     "theme": "fusion"
                 },
-                "data": [
-                    {
-                        "label": "Apache",
-                        "value": "32647479"
-                    },
-                    {
-                        "label": "Microsoft",
-                        "value": "22100932"
-                    },
-                    {
-                        "label": "Zeus",
-                        "value": "14376"
-                    },
-                    {
-                        "label": "Other",
-                        "value": "18674221"
-                    }
-                ]
+                "data": data
             }
-        }
+        })
+        // END of setState
+    }
+    render() {
+
 
         return (
             <ReactFC
@@ -57,4 +59,4 @@ class FolioPieChart extends React.Component {
     }
 }
 
-export default FolioPieChart;
+export default FolioPie;
