ada173416d942d5f1224998e9fd31dbc4a6b83e4
sgenini <sgenini@gmail.com>
2019-02-06 11:05:47 -0800

    QuickPortfolio.js fix with new ritesh code

--- client/src/components/Portfolio/QuickPortfolio.js
+++ client/src/components/Portfolio/QuickPortfolio.js
@@ -8,43 +8,84 @@ import 'jspdf-autotable';
 
 export default class QuickPortfolio extends Component {
     state = {
-        watchList:{},
-        portfolio:[]
+        watchList: {},
+        portfolio: [],
+        updatePortfolio:[],
+        userPortfolio:[],
+        totalValue:0
+
+        // chartData: []
     }
-    componentDidMount(){
-            API.getPertucularUserWatchList().then((res) => {
-                console.log(res.data);
-                this.setState({watchList:res.data});
-                console.log(this.state.watchList.stock);
-            }).then(() => {
-                console.log("dot then function ran in portfolio");
-                this.getUserPortfolio();
-            });
-        }
+    componentDidMount() {
+        API.getPertucularUserWatchList().then((res) => {
+            this.setState({ watchList: res.data });
+        }).then(() => {
+            this.getUserPortfolio();
+        });
+    }
     //}
-    
+
     //API AJAX Call to user portfolio table 
+    // getUserPortfolio = () => {
+    //     API.getUserPortfolioData(this.state.watchList.UserId).then((res) => {
+    //         this.setState({portfolio: res.data})
+    //         this.filterPortfolioData(res.data)
+    //     })
+    // }
+
+    // filterPortfolioData = (dataArray) => {
+    //     let primedArray = []
+    //     for (var i = 0; i < dataArray.length; i++) {
+    //         if (dataArray[i].quantity > 0) {
+    //             primedArray.push(dataArray[i]);
+    //         } else { };
+    //     }
+    //     this.setState({ portfolio: primedArray });
+    //     console.log(this.state.portfolio);
+    // }
+
+    //API AJAX Call to user transaction table and generate user portfolio updated new table to get profit loss 
     getUserPortfolio = () => {
         API.getUserPortfolioData(this.state.watchList.UserId).then((res) => {
-            console.log("front portfolio route hit");
-            // this.setState({portfolio: res.data})
-            console.log(this.state.portfolio)
-            this.filterPortfolioData(res.data)
+            console.log(res.data);
+            //this.setState({portfolio: res.data})
+            this.setState({ updatePortfolio: res.data })
+            const updatePort = res.data;
+            const updateWatchList = [];
+            this.state.updatePortfolio.forEach(function (el) {
+                updateWatchList.push(el.symbol)
+                //return updateWatchList
+            })
+
+            //const userWatchlist = this.state.updatePortfolio[this.state.updatePortfolio.length-1].symbols.join(",");
+            //console.log(updateWatchlist.join(","));
+            API.batchStock(updateWatchList).then((res) => {
+                //console.log(this.state.updatePortfolio);
+                //   console.log(res.data);
+                for (let k in res.data) {
+                    //        console.log(k, res.data[k].quote.latestPrice);
+                    for (let i = 0; i < updatePort.length; i++) {
+                        if (k.toLowerCase() === updatePort[i].symbol) {
+                            updatePort[i]["latestPriceIEX"] = res.data[k].quote.latestPrice;
+                        }
+                    }
+               }
+            //   console.log(updatePort);
+                 this.setState({userPortfolio: updatePort})
+                 this.totalPortfolio();
+             //console.log(price);
+
+            })
         })
     }
-    filterPortfolioData = (dataArray) => {
-        console.log(dataArray);
-        console.log("above is the data");
-        let primedArray = []
-        for (var i=0; i<dataArray.length; i++){
-            // console.log(dataArray[i].symbol)
-            if(dataArray[i].quantity > 0){
-                    primedArray.push(dataArray[i]);
-                    console.log(primedArray)
-            }else{};
-        }
-        this.setState({portfolio: primedArray});
-        console.log(primedArray);
+
+    totalPortfolio = () => {
+         let totalStockValue = 0;
+        this.state.userPortfolio.forEach(function(el){
+            totalStockValue +=(parseInt(el.Stockquantity)*parseFloat(el.latestPriceIEX));
+        })
+        this.setState({totalValue:totalStockValue});
+        console.log(this.state.totalValue);
     }
 
     //Logout User Link 
@@ -52,7 +93,6 @@ export default class QuickPortfolio extends Component {
         //this.intervalClear();
         localStorage.removeItem("loggedIn");
         API.signOutUser().then((res) => {
-            console.log(res);
         }).catch(err => console.log(err));
     }
     //Go to Home page
@@ -64,16 +104,16 @@ export default class QuickPortfolio extends Component {
         this.props.history.push("/transactions");
     }
 
-    printDocument =() =>{
+    printDocument = () => {
         const input = document.getElementById('mytable');
         html2canvas(input)
-          .then((canvas) => {
-            const imgData = canvas.toDataURL('image/png',1.0);
-            const pdf = new jsPDF('p','mm');
-            pdf.addImage(imgData, 'PNG', 10, 10);
-            // pdf.output('dataurlnewwindow');
-            pdf.save("myDocument.pdf");
-          });
+            .then((canvas) => {
+                const imgData = canvas.toDataURL('image/png', 1.0);
+                const pdf = new jsPDF('p', 'mm');
+                pdf.addImage(imgData, 'PNG', 10, 10);
+                // pdf.output('dataurlnewwindow');
+                pdf.save("myDocument.pdf");
+            });
     }
 
     render (){
@@ -82,7 +122,7 @@ export default class QuickPortfolio extends Component {
           <div className="container">
              <div className="row">
                <div className="col-md-12">
-               {(this.state.portfolio).length === 0 ? (<div>Portfolio will appear here.</div>) : (
+               {(this.state.userPortfolio).length === 0 ? (<div>Portfolio will appear here.</div>) : (
                 <table id="mytable" className="table table-striped">
                  <thead className="table-dark">
                      <tr>
@@ -95,11 +135,11 @@ export default class QuickPortfolio extends Component {
                      </tr>
                  </thead>
                  <tbody>
-                     {this.state.portfolio.map((data) => {
+                     {this.state.userPortfolio.map((data) => {
                          return (
                             <tr>
                             <td><b>{data.symbol.toUpperCase()}</b></td>
-                            <td>{data.quantity}</td>
+                            <td>{data.Stockquantity}</td>
                             {/* <td>{(data.buy) ? ("Buy") : ("Sell")}</td> */}
                             {/* <td>{data.purchasePrice}</td> */}
                             {/* <td>{data.purchaseTotal}</td> */}
